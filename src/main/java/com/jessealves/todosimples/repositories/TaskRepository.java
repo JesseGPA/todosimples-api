4fa15605b7858ffaa824cb1796399634a5fdfe4c
Jesse Alves <jesse_jgpa@hotmail.com>
2023-04-26 16:33:11 -0300

    Criação dos repositórios para User e Task

---
+++ src/main/java/com/jessealves/todosimples/repositories/TaskRepository.java
@@ -0,0 +1,24 @@
+package com.jessealves.todosimples.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jessealves.todosimples.models.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    
+    // Padrão do Jpa
+    List<Task> findByUser_Id(Long id);
+
+    // Usando JPQL
+    //@Query(value = "SELECT t FROM Task t WHERE t.user.id = :id")
+    //List<Task> findByUser_Id(@Param("id") Long id);
+
+    // Usando query nativa do SQL
+    //@Query(value = "SELECT * FROM task t WHERE t.user_id = :id", nativeQuery = true)
+    //List<Task> findByUser_Id(@Param("id") Long id);
+
+}

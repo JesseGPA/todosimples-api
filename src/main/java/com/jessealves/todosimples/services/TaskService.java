fd056c5f2ede1380c44d745881f12d0fb3aa4b6a
Jesse Alves <jesse_jgpa@hotmail.com>
2023-04-27 22:57:22 -0300

    Criação do ExceptionHandler

--- src/main/java/com/jessealves/todosimples/services/TaskService.java
+++ src/main/java/com/jessealves/todosimples/services/TaskService.java
@@ -10,6 +10,8 @@ import org.springframework.transaction.annotation.Transactional;
 import com.jessealves.todosimples.models.Task;
 import com.jessealves.todosimples.models.User;
 import com.jessealves.todosimples.repositories.TaskRepository;
+import com.jessealves.todosimples.services.exceptions.DataBindingViolationException;
+import com.jessealves.todosimples.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class TaskService {
@@ -23,7 +25,7 @@ public class TaskService {
     public Task findById(Long id) {
         Optional<Task> task = this.taskRepository.findById(id);
 
-        return task.orElseThrow(() -> new RuntimeException(
+        return task.orElseThrow(() -> new ObjectNotFoundException(
             "Tarefa não encontrada! Id:" + id + ", Tipo:" + Task.class.getName()
         ));
     }
@@ -56,7 +58,7 @@ public class TaskService {
         try {
             this.taskRepository.deleteById(id);
         } catch (Exception e) {
-            throw new RuntimeException("Não é possível excluir pois há entidades relacionadas");
+            throw new DataBindingViolationException("Não é possível excluir pois há entidades relacionadas");
         }
     }
 }
--- src/main/java/com/jessealves/todosimples/services/UserService.java
+++ src/main/java/com/jessealves/todosimples/services/UserService.java
@@ -9,6 +9,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.jessealves.todosimples.models.User;
 import com.jessealves.todosimples.repositories.UserRepository;
+import com.jessealves.todosimples.services.exceptions.DataBindingViolationException;
+import com.jessealves.todosimples.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -19,7 +21,7 @@ public class UserService {
     public User findById(Long id) {
         Optional<User> user = this.userRepository.findById(id);
 
-        return user.orElseThrow(() -> new RuntimeException(
+        return user.orElseThrow(() -> new ObjectNotFoundException(
             "Usuário não encontrado! Id: " + id + ", Tipo: " + User.class.getName()
         ));
     }
@@ -46,7 +48,7 @@ public class UserService {
         try {
             this.userRepository.deleteById(id);
         } catch (Exception e) {
-            throw new RuntimeException("Não é possível excluir pois há entidades relacionadas");
+            throw new DataBindingViolationException("Não é possível excluir pois há entidades relacionadas");
         }
     }
 }

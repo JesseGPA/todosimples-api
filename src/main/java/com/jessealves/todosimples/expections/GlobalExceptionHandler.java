3c368d3e5117dd92fc5509b8bcd934ee2999d722
Jesse Alves <jesse_jgpa@hotmail.com>
2023-04-27 22:58:05 -0300

    Criação do ExceptionHandler - novas classes

---
+++ src/main/java/com/jessealves/todosimples/expections/GlobalExceptionHandler.java
@@ -0,0 +1,112 @@
+package com.jessealves.todosimples.expections;
+
+import javax.validation.ConstraintViolationException;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.jessealves.todosimples.services.exceptions.DataBindingViolationException;
+import com.jessealves.todosimples.services.exceptions.ObjectNotFoundException;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestControllerAdvice
+@Slf4j(topic = "GLOBAL_EXCEPTION_HANDLER")
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    
+    @Override
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+                MethodArgumentNotValidException methodArgumentNotValidException,
+                HttpHeaders headers,
+                HttpStatus status,
+                WebRequest request
+    ) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.UNPROCESSABLE_ENTITY.value(), "Erro de validação. Verifique o campo 'errors' para mais detalhes.");
+        for(FieldError fieldError : methodArgumentNotValidException.getBindingResult().getFieldErrors()) {
+            errorResponse.addValidationErro(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        return ResponseEntity.unprocessableEntity().body(errorResponse);
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<Object> handleAllUncaughtException(
+            Exception exception,
+            WebRequest request
+    ) {
+        final String errorMessage = "Ocorreu um erro desconhecido";
+        log.error(errorMessage, exception);
+
+        return buildErrorResponse(exception, errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ResponseEntity<Object> handleDataIntegrityViolationException(
+            DataIntegrityViolationException dataIntegrityViolationException,
+            WebRequest request
+    ) {
+        String errorMessage = dataIntegrityViolationException.getMostSpecificCause().getMessage();
+        log.error("Falha ao salvar uma entidade com problemas de integridade: " + errorMessage, dataIntegrityViolationException);
+
+        return buildErrorResponse(dataIntegrityViolationException, errorMessage, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ResponseEntity<Object> handleConstraintViolationException(
+            ConstraintViolationException constraintViolationException,
+            WebRequest request
+    ) {
+        String errorMessage = constraintViolationException.getMessage();
+        log.error("Falha ao salvar uma entidade com problemas de integridade: " + errorMessage, constraintViolationException);
+
+        return buildErrorResponse(constraintViolationException, errorMessage, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<Object> handleObjectNotFoundException(
+            ObjectNotFoundException objectNotFoundException,
+            WebRequest request
+    ) {
+        String errorMessage = objectNotFoundException.getMessage();
+        log.error("Falha ao encontrar um elemento: " + errorMessage, objectNotFoundException);
+
+        return buildErrorResponse(objectNotFoundException, errorMessage, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(DataBindingViolationException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ResponseEntity<Object> handleObjectNotFoundException(
+            DataBindingViolationException dataBindingViolationException,
+            WebRequest request
+    ) {
+        String errorMessage = dataBindingViolationException.getMessage();
+        log.error("Falha ao salvar elemento que possui dados associados: " + errorMessage, dataBindingViolationException);
+
+        return buildErrorResponse(dataBindingViolationException, errorMessage, HttpStatus.CONFLICT);
+    }
+    
+    private ResponseEntity<Object> buildErrorResponse(
+                Exception exception,
+                String message,
+                HttpStatus httpStatus
+    ) {    
+        ErrorResponse errorResponse = new ErrorResponse(httpStatus.value(), message);
+
+        return ResponseEntity.status(httpStatus).body(errorResponse);
+    }
+
+}
---
+++ src/main/java/com/jessealves/todosimples/services/exceptions/DataBindingViolationException.java
@@ -0,0 +1,14 @@
+package com.jessealves.todosimples.services.exceptions;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class DataBindingViolationException extends DataIntegrityViolationException {
+
+    public DataBindingViolationException(String message) {
+        super(message);
+    }
+    
+}
